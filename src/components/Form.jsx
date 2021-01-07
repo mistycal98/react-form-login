62c6f7077388b3fad09e53b09280de5091f0ea3a
mystical98 <tushar.mistry@raweng.com>
2021-01-07 20:47:48 +0530

    Logging sucessfully done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,56 +1,65 @@
 import { useState } from "react";
 import { authUrl } from "../api/apiCall";
+import styles from "./Form.module.scss";
+
+import { Cookies } from "js-cookie";
 
 export default function Form() {
-  // State Initialize
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	// State Initialize
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	// const [err, setError] = useState(false);
 
-  // Handling onChange Event
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
+	// Handling onChange Event
+	const handleEmail = (event) => {
+		setEmail(event.target.value);
+	};
 
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
+	const handlePassword = (event) => {
+		setPassword(event.target.value);
+	};
 
-  // handle onSubmit Event
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = JSON.stringify({
-      email,
-      password,
-    });
-    console.log(data);
-    try {
-      let response = await fetch(`${authUrl}/users/login`, {
-        method: "POST",
-        body: data,
-        redirect: "follow",
-        header: JSON.stringify({
-          "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pc3RyeS50dXNoYXI5OEBnbWFpbC5jb20iLCJpYXQiOjE2MTAwMTQzNjYsImV4cCI6MTYxMDEwMDc2Nn0.z9GRdpVI_dAhkQM2Dd71IzEfolASE8T1T3hrqdrLa-U",
-          "Content-type": "application/json",
-          "Cookie":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pc3RyeS50dXNoYXI5OEBnbWFpbC5jb20iLCJpYXQiOjE2MTAwMTQzNjYsImV4cCI6MTYxMDEwMDc2Nn0.z9GRdpVI_dAhkQM2Dd71IzEfolASE8T1T3hrqdrLa-U",
-        }),
-      });
+	// handle onSubmit Event
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			let response = await fetch(`${authUrl}/users/login`, {
+				method: "POST",
+				body: JSON.stringify({
+					email: email,
+					password: password,
+				}),
+				redirect: "follow",
+				headers: {
+					"Content-type": "application/json",
+				},
+			});
 
-      let result = await response.json();
-      console.log(result);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+			let result = await response.json();
+			console.log(result);
+			console.log(result.data[0].jwt);
+			Cookies.set("jwt", result.data[0].jwt);
+		} catch (err) {
+			event.preventDefault();
+			// setError(true);
+			console.log(err);
+		}
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">email:</label>
-      <input type="text" name="email" value={email} onChange={handleEmail} />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" value={password} onChange={handlePassword} />
-      <button>Login</button>
-    </form>
-  );
+	return (
+		<div className={styles.form}>
+			<h1>Login</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="email">Email : </label>
+					<input type="text" name="email" value={email} onChange={handleEmail} />
+				</div>
+				<div>
+					<label htmlFor="password">Password : </label>
+					<input type="password" name="password" value={password} onChange={handlePassword} />
+				</div>
+				<button>Login</button>
+			</form>
+		</div>
+	);
 }
