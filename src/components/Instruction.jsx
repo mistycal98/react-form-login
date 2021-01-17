6f086d684c46dba3367f53dbe7cfa5984a28a250
mystical98 <tushar.mistry@raweng.com>
2021-01-17 20:45:44 +0530

    Addedd instructions

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./styles/Instruction.module.scss";
 export default function Instruction() {
-  return (
-    <div className={styles.instruction}>
-      <h1>Todo List App</h1>
-      <ul>
-        <li>
-          Register : First Register from Navbar. On Sucessfull Registeration you will be redirected
-          to login Page
-        </li>
-        <li>Login : Login with the email and password</li>
-        <li>On Sucessfull Login redirected to the dashboard page with Todo App</li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className={styles.instruction}>
+			<h1>Todo List App</h1>
+			<ul>
+				<li>
+					First Submit may take time because server is hosted on heroku where container
+					initialization takes time.
+				</li>
+				<li>
+					Register : First Register from Navbar. On Sucessfull Registeration you will be redirected
+					to login Page
+				</li>
+				<li>Login : Login with the email and password</li>
+				<li>On Sucessfull Login redirected to the dashboard page with Todo App</li>
+			</ul>
+		</div>
+	);
 }
